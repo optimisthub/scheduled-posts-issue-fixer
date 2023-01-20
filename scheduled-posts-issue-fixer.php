9d37116415d443d85460d6906502dfce4309380c
Fatih Toprak <patron@fatihtoprak.com>
2023-01-21 00:36:14 +0300

    initial commit.

---
+++ scheduled-posts-issue-fixer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Scheduled Posts Issue Fixer
+ * Plugin URI:      https://github.com/optimisthub/scheduled-posts-issue-fixer
+ * Description:     This plugin does one thing and does it well: it fixes the missed schedule error and triggers your scheduled posts to publish on time. We’ve developed this post scheduler plugin with performance in mind, so it won’t affect the speed or performance of your website.
+ * Author:          @optimisthub
+ * Author URI:      https://optimisthub.com
+ * Text Domain:     scheduled-posts-issue-fixer
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Scheduled_Posts_Issue_Fixer
+ */
+
+// Your code starts here.
