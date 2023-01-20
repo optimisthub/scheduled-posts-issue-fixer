943c9928f28562a25caba2f04f7cc91cc9a5d3e8
Fatih Toprak <patron@fatihtoprak.com>
2023-01-21 01:30:34 +0300

    Readme completed.

--- scheduled-posts-issue-fixer.php
+++ scheduled-posts-issue-fixer.php
@@ -6,9 +6,9 @@
  * Author:          @optimisthub
  * Author URI:      https://optimisthub.com
  * Text Domain:     scheduled-posts-issue-fixer 
- * Version:         0.1.0
+ * Version:         1.0.0
  * Requires at least: 5.0
- * Tested up to: 6.0
+ * Tested up to: 6.1.1
  * Requires PHP: 7.1
  * License: GPLv2
  */
