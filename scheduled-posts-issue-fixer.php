4d9aff0f42c575f804ea81c419d62ea1027c4e52
Fatih Toprak <patron@fatihtoprak.com>
2023-02-07 02:02:11 +0300

    new github action steps.

--- scheduled-posts-issue-fixer.php
+++ scheduled-posts-issue-fixer.php
@@ -6,7 +6,7 @@
  * Author:          optimisthub
  * Author URI:      https://optimisthub.com
  * Text Domain:     scheduled-posts-issue-fixer 
- * Version:         1.0.6
+ * Version:         1.0.7
  * Requires at least: 5.0
  * Tested up to: 6.1.1
  * Requires PHP: 7.1
